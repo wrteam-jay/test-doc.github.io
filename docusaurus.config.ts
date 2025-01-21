5cecde980b58843fb490fa3565a71e1acb4520c4
Jay Parmar <wrteam.jay@gmail.com>
2025-01-21 10:40:51 +0530

    renove doc ref

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,13 +48,6 @@ const config: Config = {
         src: 'img/logo.svg',
       },
       items: [
-        // {
-        //   type: 'docSidebar',
-        //   sidebarId: 'tutorialSidebar',
-        //   position: 'left',
-        //   label: 'Tutorial',
-        // },
-        // {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -94,10 +87,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
